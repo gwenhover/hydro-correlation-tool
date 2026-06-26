8fafc24a46805b92cafbec13cc83698d549b7e3a
Gwen Hover <ghover@aquaveo.com>
2026-06-26 09:55:49 -0600

    Initial scaffold of hydro-correlation-tool

---
+++ tethysapp/hydro_correlation_tool/app.py
@@ -0,0 +1,17 @@
+from tethys_sdk.base import TethysAppBase
+
+
+class App(TethysAppBase):
+    """
+    Tethys app class for Hydro Correlation Tool.
+    """
+    name = 'Hydro Correlation Tool'
+    description = 'The Hydro Correlation Tool is a single-user scientific workbench for building and maintaining a high-quality cross-mapping table that links each active USGS streamflow gage to orresponding NWM v3 reach and GEOGLOWS v2 river.'
+    package = 'hydro_correlation_tool'  # WARNING: Do not change this value
+    index = 'home'
+    icon = f'{package}/images/icon_gage_on_river.svg'
+    root_url = 'hydro-correlation-tool'
+    color = '#002E5D'
+    tags = '"NWM","GEOGLOWS","USGS Gages"'
+    enable_feedback = False
+    feedback_emails = []
